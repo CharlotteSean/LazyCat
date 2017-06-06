b5de2b349f2b6897d7fa839f942c4612725ce60e
hicoldcat <1351290389@qq.com>
2017-06-06 20:44:39 +0800

    Complete login screen style

---
+++ app/assets/style/baseStyle.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+const baseStyle = StyleSheet.create({
+    baseText: {
+        color:"#ffffff",
+        fontFamily:'fangzhengkaitijianti',
+        fontSize:20,
+    },
+    basePromptText: {
+        color:"#ffffff",
+        fontFamily:'fangzhengkaitijianti',
+        fontSize:16,
+    }
+})
+
+export {baseStyle}
--- app/screen/LoginScreen.js
+++ app/screen/LoginScreen.js
@@ -1,22 +1,73 @@
 import React, {Component} from 'react';
 import {
-    Platform,
     StyleSheet,
     Text,
     View,
-    ViewPagerAndroid,
     ImageBackground,
-    Dimensions,
-    PixelRatio
+    TextInput,
+    Button,
+    TouchableOpacity
 } from 'react-native';
 
+import {baseStyle} from '../assets/style/baseStyle'
 import {getWidthByPercent, getHeightByPercent} from '../assets/uitl/baseTool'
 
+
 export default class LoginScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName:'',
+            passWord:'',
+        };
+    }
+
+    _onLogin () {
+
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <ImageBackground source={require('../assets/images/welcome_bg1.jpeg')} style={styles.loginBgImg}>
+
+                    {/* 标题 */}
+                    <View style={styles.titleContainer}>
+                        <Text style={[baseStyle.baseText, styles.titleContent]}>LOGO</Text>
+                    </View>
+
+                    {/* 登陆项 */}
+                    <View style={styles.loginContainer}>
+                        <TextInput
+                            style={[baseStyle.baseText,styles.loginItem,styles.userName]}
+                            onChangeText={(text) => this.setState({userName:text})}
+                            placeholder='用户名'
+                            placeholderTextColor='#ffffff'
+                            underlineColorAndroid="transparent"
+                            blurOnSubmit={true}
+                            value={this.state.userName}>
+                        </TextInput>
+                        <TextInput
+                            style={[baseStyle.baseText,styles.loginItem,styles.passWord]}
+                            onChangeText={(text) => this.setState({passWord:text})}
+                            secureTextEntry={true}
+                            placeholder='密码'
+                            placeholderTextColor='#ffffff'
+                            underlineColorAndroid="transparent"
+                            blurOnSubmit={true}
+                            inlineImageLeft='../assets/images/welcome_bg1.jpeg'
+                            value={this.state.passWord}>
+                        </TextInput>
+                        <TouchableOpacity onPress={this._onLogin} style={styles.loginBtn} activeOpacity={0.9}>
+                            <Text style={[baseStyle.baseText,styles.button]}>登录</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    {/* 注册账号和忘记密码 */}
+                    <View style={styles.accountContainer}>
+                        <Text style={baseStyle.basePromptText}>注册账号</Text>
+                        <Text style={baseStyle.basePromptText}>忘记密码</Text>
+                    </View>
 
                 </ImageBackground>
             </View>
@@ -34,11 +85,54 @@ const styles = StyleSheet.create({
     loginBgImg: {
         width: getWidthByPercent(100),
         height: getHeightByPercent(100),
-        zIndex: 100
+        flexDirection:'column',
     },
-    welcomeInfo: {
-        zIndex: 1000,
-        fontSize: 30,
-        color: '#000000'
-    }
+    titleContainer: {
+        marginTop:getHeightByPercent(10),
+    },
+    titleContent: {
+        textAlign:"center",
+        fontSize:50,
+    },
+    loginContainer: {
+        width:getWidthByPercent(100),
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    loginItem: {
+        height: 40,
+        width:getWidthByPercent(80),
+        borderBottomWidth:1,
+        borderColor:'#FFFFFF',
+        padding: 10,
+        fontSize:18,
+    },
+    userName: {
+        marginTop:getHeightByPercent(35)
+    },
+    passWord: {
+        marginTop:getHeightByPercent(2)
+    },
+    loginBtn: {
+        width:getWidthByPercent(80),
+        height: 40,
+        marginTop:getHeightByPercent(5),
+        backgroundColor:'#4C9FF8',
+        alignItems:'center',
+        justifyContent:'center',
+        flexDirection:'row',
+    },
+    button: {
+        textAlign:'center',
+
+    },
+    accountContainer: {
+        width:getWidthByPercent(100),
+        marginTop:getHeightByPercent(2),
+        justifyContent:'space-between',
+        alignItems:'center',
+        flexDirection:'row',
+        paddingLeft:getWidthByPercent(10),
+        paddingRight:getWidthByPercent(10),
+    },
 })
